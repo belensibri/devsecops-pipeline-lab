34f695c886c4a231dca4493142d12f99b3cfa323
belensibri <belensibrian16@gmail.com>
2026-09-04 19:17:01 -0600

    Agregar infraestructura Terraform

---
+++ infra/variables.tf
@@ -0,0 +1,17 @@
+variable "aws_region" { 
+
+  type    = string 
+
+  default = "us-east-1" 
+
+} 
+
+  
+
+variable "bucket_name" { 
+
+  description = "devsecops-lab-perezsib-2026" 
+
+  type        = string 
+
+} 
